e0fe1174e031c22b1b14b269625d774139cf79cf
anshu-sahani <avinashsahani900@gmail.com>
2024-08-18 22:54:26 +0530

    comparision push on

---
+++ 01_basics/oprators.js
@@ -0,0 +1,15 @@
+// oparators
+// postfix
+ // X++
+ let x = 5;
+ let y = x++;
+ console.log(x);
+ console.log(y);
+ 
+// prefix
+ let a = 5;
+ let b = ++a;
+ console.log(a);
+ console.log(b);
+ 
+ 
