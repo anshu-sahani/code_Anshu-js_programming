782e00b4f89e12b71650f610455cfb3c847f2d42
anshu-sahani <avinashsahani900@gmail.com>
2024-08-20 19:13:31 +0530

    string file push on

---
+++ 01_basics/string.js
@@ -0,0 +1,35 @@
+const name = 'anshu';
+const age = 56;
+
+//console.log(`My name is ${name} sahni and i am ${age} years old.`);
+
+let newString = new String('my name is anshu sahani');
+
+//console.log(newString);
+
+// console.log(newString[0,8]);
+// console.log(newString.length);
+// console.log(newString.substring(0, 10).toUpperCase());
+// console.log(newString.anchor('anshu'));
+// console.log(newString.at()); // return the value of charector when i passes at() then.
+// console.log(newString.big());
+// console.log(newString.blink());
+// console.log(newString.bold());
+// console.log(newString.charAt(5));
+// console.log(newString.charCodeAt()); //Represent charector code 
+// console.log(newString.codePointAt());
+// console.log(newString.concat(' ', name));// join many string of concat function
+// console.log(newString.includes('hello worls'));
+
+console.log(newString.slice(2, 21));
+
+console.log(newString.split(' '));
+
+
+
+
+
+
+
+
+
